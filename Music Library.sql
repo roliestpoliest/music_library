710acafd65f47ba141ba9c68922fa7bfa3553d27
carolyn <41408998+roliestpoliest@users.noreply.github.com>
2024-02-29 17:16:00 -0600

    constraint and variable name updates

--- Music Library.sql
+++ Music Library.sql
@@ -1,14 +1,14 @@
 DROP TABLE IF EXISTS `accounts`;
 CREATE TABLE `accounts` (
   `account_id` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  `user_role` ENUM('User', 'Artist', 'Admin') NOT NULL DEFAULT 'User',
+  `user_role` ENUM('User', 'Artist', 'Admin') NOT NULL,
   `name` VARCHAR(255) NOT NULL,
   `username` VARCHAR(255) NOT NULL,
   `bio` TEXT,
   `gender` CHAR,
   `DOB` DATE,
   `region` VARCHAR(255),
-  `is_student` BOOLEAN DEFAULT FALSE,
+  `is_student` BOOLEAN,
   `email` VARCHAR(255) UNIQUE NOT NULL,
   `password` VARCHAR(255) NOT NULL
 );
@@ -58,20 +58,19 @@ DROP TABLE IF EXISTS `albums`;
 CREATE TABLE `albums` (
   `album_id` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   `title` VARCHAR(255) DEFAULT "Untitled Album",
-  `format` ENUM('Album', 'Single', 'EP', 'LP', 'SP'),
+  `format` ENUM('Album', 'Single', 'EP', 'LP', 'SP') NOT NULL,
   `release_date` DATE,
   `rating` INT CHECK (rating >= 0 AND rating <= 5),
   `artist_id` INT UNSIGNED,
   `record_label_id` INT UNSIGNED,
   CONSTRAINT `fk_albums_artists` FOREIGN KEY (`artist_id`) REFERENCES `artists` (`artist_id`),
-  CONSTRAINT `fk_albums_record_labels` FOREIGN KEY (`record_label_id`) REFERENCES `record_labels` (`record_label_id`),
+  CONSTRAINT `fk_albums_record_labels` FOREIGN KEY (`record_label_id`) REFERENCES `record_labels` (`record_label_id`)
 );
 
 DROP TABLE IF EXISTS `songs`;
 CREATE TABLE `songs` (
   `song_id` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   `title` VARCHAR(255) DEFAULT "Untitled Song",
-  -- `audio_format` ENUM('MP3', 'M4A', 'WAV') NOT NULL,
   `duration` INT,
   `listens` INT,
   `rating` INT CHECK (rating >= 0 AND rating <= 5),
@@ -88,22 +87,13 @@ CREATE TABLE `song_associations` (
   CONSTRAINT `fk_song_associations_artist` FOREIGN KEY (`artist_id`) REFERENCES `artists` (`artist_id`)
 );
 
-DROP TABLE IF EXISTS `song_to_album`;
-CREATE TABLE song_album (
+DROP TABLE IF EXISTS `songs_to_album`;
+CREATE TABLE `songs_to_album` (
   `song_id` INT UNSIGNED,
   `album_id` INT UNSIGNED,
   PRIMARY KEY(`song_id`, `album_id`),
-  CONSTRAINT `fk_song_album_song` FOREIGN KEY (`song_id`) REFERENCES `songs` (`song_id`),
-  CONSTRAINT `fk_song_album_album` FOREIGN KEY (`album_id`) REFERENCES `albums` (`album_id`)
-);
-
-DROP TABLE IF EXISTS `liked_songs`;
-CREATE TABLE `liked_songs` (
-  `account_id` INT UNSIGNED,
-  `song_id` INT UNSIGNED,
-  PRIMARY KEY(`account_id`, `song_id`),
-  CONSTRAINT `fk_liked_songs_accounts` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`account_id`),
-  CONSTRAINT `fk_liked_songs_songs` FOREIGN KEY (`song_id`) REFERENCES `songs` (`song_id`)
+  CONSTRAINT `fk_songs_to_album_song` FOREIGN KEY (`song_id`) REFERENCES `songs` (`song_id`),
+  CONSTRAINT `fk_songs_to_album_album` FOREIGN KEY (`album_id`) REFERENCES `albums` (`album_id`)
 );
 
 DROP TABLE IF EXISTS `followed_artists`;
@@ -120,32 +110,31 @@ CREATE TABLE `playlists` (
   `playlist_id` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   `account_id` INT UNSIGNED,
   `title` VARCHAR(255) DEFAULT "Unititled Playlist",
-  CONSTRAINT `fk_playlists_accounts` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`account_id`),
+  CONSTRAINT `fk_playlists_accounts` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`account_id`)
 );
 
-DROP TABLE IF EXISTS `song_to_playlist`;
-CREATE TABLE song_album (
+DROP TABLE IF EXISTS `songs_to_playlist`;
+CREATE TABLE `songs_to_playlist` (
   `song_id` INT UNSIGNED,
   `playlist_id` INT UNSIGNED,
   PRIMARY KEY(`song_id`, `playlist_id`),
-  CONSTRAINT `fk_song_album_song` FOREIGN KEY (`song_id`) REFERENCES `songs` (`song_id`),
-  CONSTRAINT `fk_song_playlist_playlist` FOREIGN KEY (`playlist_id`) REFERENCES `playlists` (`playlist_id`)
+  CONSTRAINT `fk_songs_to_playlist_song` FOREIGN KEY (`song_id`) REFERENCES `songs` (`song_id`),
+  CONSTRAINT `fk_songs_to_playlist_playlist` FOREIGN KEY (`playlist_id`) REFERENCES `playlists` (`playlist_id`)
 );
-
 
 DROP TABLE IF EXISTS `subscription_plans`;
 CREATE TABLE `subscription_plans` (
   `subscription_plan_id` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  `subscription_plan_type` ENUM('Free', 'Individual', 'Student') DEFAULT 'Free',
+  `subscription_plan_type` ENUM('Free', 'Individual', 'Student') NOT NULL,
   `description` TEXT,
   `term_days_length` INT NOT NULL,
   `price` FLOAT NOT NULL
 );
 
-DROP TABLE IF EXISTS `active_subscriptions`;
+DROP TABLE IF EXISTS `subscriptions`;
 CREATE TABLE `subscriptions` (
   `subscription_id` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  `start_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `start_date` TIMESTAMP NOT NULL,
   `end_date` TIMESTAMP NOT NULL,
   `account_id` INT UNSIGNED,
   `subscription_plan_id` INT UNSIGNED,
@@ -156,7 +145,7 @@ CREATE TABLE `subscriptions` (
 DROP TABLE IF EXISTS `transactions`;
 CREATE TABLE `transactions` (
   `transaction_id` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  `payment_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `payment_date` TIMESTAMP NOT NULL,
   `payment_source` VARCHAR(16) NOT NULL,
   `total` FLOAT NOT NULL,
   `account_id` INT UNSIGNED,
