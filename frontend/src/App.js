9e26706d82c402be4d1974accb59083c904333e2
carolyn <41408998+roliestpoliest@users.noreply.github.com>
2024-03-11 02:36:47 -0500

    ✨ Created Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 import Sidebar from "./components/Sidebar/Sidebar";
+import Navbar from "./components/Navbar/Navbar";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Sidebar />
-      <div>
+      <Navbar />
+      {/* <div>
         rest of website :)
-      </div>
+      </div> */}
     </div>
   );
 }
