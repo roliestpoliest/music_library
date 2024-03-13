7789ab7dea4d85134c2c0f4ffabbac340e51db35
carolyn <41408998+roliestpoliest@users.noreply.github.com>
2024-03-13 04:03:51 -0500

    Fix CSS styling in AlbumCard and ArtistCard components

--- frontend/src/components/ArtistCard/ArtistCard.js
+++ frontend/src/components/ArtistCard/ArtistCard.js
@@ -5,7 +5,8 @@ export default function ArtistCard() {
   return (
     <div className="art-card">
       <div className="art-card-image">
-        <img className="artist-image"
+        <img
+          className="artist-image"
           src="https://storage.googleapis.com/inbox-zero/2880x/saad-chaudhry-G25LeMV7fAw-unsplash.jpg"
           alt="album cover"
         />
