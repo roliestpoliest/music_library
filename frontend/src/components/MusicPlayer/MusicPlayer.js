5ca3b251f54131377e3fd4554edfa666a4b77811
carolyn <41408998+roliestpoliest@users.noreply.github.com>
2024-03-12 21:24:27 -0500

    ✨ Add MusicPlayer component

---
+++ frontend/src/components/MusicPlayer/MusicPlayer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './MusicPlayer.css'
+
+export default function MusicPlayer() {
+  return (
+    <div className='MusicPlayer'>
+      <div>Music Here</div>
+      <div>Play Here</div>
+      <div>Audio Here</div>
+    </div>
+  )
+}
