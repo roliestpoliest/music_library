f64e386b4583939631e6eaceb15b93d978509281
carolyn <41408998+roliestpoliest@users.noreply.github.com>
2024-03-13 04:14:55 -0500

    ✨ Add MusicControls component to MusicPlayer

--- frontend/src/components/MusicPlayer/MusicPlayer.js
+++ frontend/src/components/MusicPlayer/MusicPlayer.js
@@ -1,12 +1,15 @@
-import React from 'react'
-import './MusicPlayer.css'
+import React from "react";
+import "./MusicPlayer.css";
+import MusicControls from "./MusicControls/MusicControls";
 
 export default function MusicPlayer() {
   return (
-    <div className='MusicPlayer'>
+    <div className="MusicPlayer">
       <div>Music Here</div>
-      <div>Play Here</div>
+      <div>
+        <MusicControls />
+      </div>
       <div>Audio Here</div>
     </div>
-  )
+  );
 }
