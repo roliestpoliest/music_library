b294bf27ec246256a9a56696cce0301db9d0756c
carolyn <41408998+roliestpoliest@users.noreply.github.com>
2024-03-13 23:25:27 -0500

    ✏️ Refactor App component and add Home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,28 @@
 import "./App.css";
-import Sidebar from "./components/Sidebar/Sidebar";
-import Navbar from "./components/Navbar/Navbar";
-import MusicPlayer from "./components/MusicPlayer/MusicPlayer";
-import AlbumCard from "./components/AlbumCard/AlbumCard";
-import ArtistCard from "./components/ArtistCard/ArtistCard";
+import React from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
 
 function App() {
   return (
-    <div className="App">
-      <div className="body">
-        <Sidebar />
-        <div className="content">
-          <Navbar />
-          <div className="display">
-            <div className="cards">
-              <AlbumCard />
-              <AlbumCard />
-              <AlbumCard />
-              <AlbumCard />
-              <AlbumCard />
-              <AlbumCard />
-              <ArtistCard />
-            </div>
-          </div>
-        </div>
-      </div>
-      <MusicPlayer />
+    <div>
+      <Routes>
+        <Route path="/Home" element={<Home />} />
+        <Route path="/About" element={<About />} />
+      </Routes>
+
+      <div className="App">Hello</div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/home">Home</Link>
+          </li>
+          <li>
+          <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,13 +1,13 @@
-import "./App.css";
-import Sidebar from "./components/Sidebar/Sidebar";
-import Navbar from "./components/Navbar/Navbar";
-import MusicPlayer from "./components/MusicPlayer/MusicPlayer";
-import AlbumCard from "./components/AlbumCard/AlbumCard";
-import ArtistCard from "./components/ArtistCard/ArtistCard";
+import "./Home.css";
+import Sidebar from "../../components/Sidebar/Sidebar";
+import Navbar from "../../components/Navbar/Navbar";
+import MusicPlayer from "../../components/MusicPlayer/MusicPlayer";
+import AlbumCard from "../../components/AlbumCard/AlbumCard";
+import ArtistCard from "../../components/ArtistCard/ArtistCard";
 
-function App() {
+function Home() {
   return (
-    <div className="App">
+    <div className="Home">
       <div className="body">
         <Sidebar />
         <div className="content">
@@ -30,4 +30,4 @@ function App() {
   );
 }
 
-export default App;
+export default Home;
