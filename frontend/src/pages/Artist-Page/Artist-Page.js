a7da044f2f9d66203f547ecd51b0ea42dfd73c3a
CyberCthulhu <csmorales2002@gmail.com>
2024-03-13 23:04:42 -0500

    created remaining pages
    created artist page, artist home page, login page, and create user page.

---
+++ frontend/src/pages/Artist-Page/Artist-Page.js
@@ -0,0 +1,25 @@
+import "./Artist-Page.css";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Navbar from "./components/Navbar/Navbar";
+import MusicPlayer from "./components/MusicPlayer/MusicPlayer";
+import AlbumCard from "./components/AlbumCard/AlbumCard";
+import ArtistCard from "./components/ArtistCard/ArtistCard";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="body">
+        <Sidebar />
+        <div className="content">
+          <Navbar />
+          <div className="display">
+            Artist information goes here
+          </div>
+        </div>
+      </div>
+      <MusicPlayer />
+    </div>
+  );
+}
+
+export default App;
