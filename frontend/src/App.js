b660d094cb255c7e5c9e5958f3ab801b155b971b
DrCrazy51 <larrymachineshop@outlook.com>
2024-03-14 02:02:29 -0500

    css changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,9 @@ import './App.css';
 //import './logintest.css';
 //<p> Username: </p>
 //<p> Password: </p>
+//<button onClick = {clickLogin}> Login </button>
+/*<button className = "forgotPassword" onClick = {forgotPassword}> Forgot Password? </button>
+      <button className = "newAccount" onClick = {newAccount}> New Account? </button> */
 function App() {
 
     const [name, setName] = useState("Username")
@@ -16,18 +19,25 @@ function App() {
     {
       setPassword(event.target.value)
     }
-    function clickLogin()
+    /*function clickLogin()
     {
       alert("Logging to home page")
     }
+    function forgotPassword()
+    {
+      alert("Heading to forgot password page")
+    }
+    function newAccount()
+    {
+      alert("Heading to new account page")
+    } */
     return (
     <div>
       <div className = 'title'>
         <p> DisCoogs </p>
       </div>
-      <input className = 'username col-xs-3' value = {name} onChange = {handleNameChange} placeholder = "Username" />
-      <input className = "password col-xs-4 col-xs-offset-2" value1 = {password} onChange = {handlePasswordChange} placeholder = "Password"/>
-      <button onClick = {clickLogin}> Login </button>
+      <input className = 'username' value = {name} onChange = {handleNameChange} placeholder = "Username" />
+      <input className = "password" value1 = {password} onChange = {handlePasswordChange} placeholder = "Password"/>
     </div>
   );
 }
