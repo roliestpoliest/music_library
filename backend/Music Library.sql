539c5d67c82c329b296af6e508193284b60efa08
carolyn <41408998+roliestpoliest@users.noreply.github.com>
2024-03-15 22:43:02 -0500

    token_storage table - add foreign key constraint

--- backend/Music Library.sql
+++ backend/Music Library.sql
@@ -1,12 +1,3 @@
-DROP DATABASE IF EXISTS `token_storage`;
-
-CREATE TABLE
-  token_storage (
-    account_id INT NOT NULL,
-    token VARCHAR(555) NOT NULL,
-    createdDate VARCHAR(255) NOT NULL
-  );
-
 DROP TABLE IF EXISTS `accounts`;
 
 CREATE TABLE
@@ -22,6 +13,17 @@ CREATE TABLE
     `region` VARCHAR(255),
     `email` VARCHAR(255) UNIQUE NOT NULL,
     `password` VARCHAR(255) NOT NULL
+  );
+
+DROP DATABASE IF EXISTS `token_storage`;
+
+CREATE TABLE
+  `token_storage` (
+    account_id INT UNSIGNED PRIMARY KEY,
+    token VARCHAR(555) NOT NULL,
+    createdDate VARCHAR(255) NOT NULL,
+    CONSTRAINT `fk_token_storage_accounts` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`account_id`)
+
   );
 
 DROP TABLE IF EXISTS `admins`;
